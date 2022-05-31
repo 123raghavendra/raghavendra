429ade295391b6052ac51471aef6c7fd73af1e2c
User <raghavendra123xworkz@gmail.com>
2022-05-31 22:53:41 +0530

    has a relationship example

---
+++ corejava/src/com/xworkz/oops/library.java
@@ -0,0 +1,13 @@
+package com.xworkz.oops;
+
+public class library {
+	String name;
+	String location;
+	  student student;
+	  
+	   
+	   
+	   
+	
+
+}
---
+++ corejava/src/com/xworkz/oops/student.java
@@ -0,0 +1,9 @@
+package com.xworkz.oops;
+
+public class student {
+	String id;
+	String name;
+	String branch;
+
+
+}
