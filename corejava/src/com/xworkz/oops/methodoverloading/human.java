1c015c70a7d29172eadc1791d1745a081064e84d
User <raghavendra123xworkz@gmail.com>
2022-06-01 12:52:51 +0530

    example of method overloading

---
+++ corejava/src/com/xworkz/oops/methodoverloading/human.java
@@ -0,0 +1,23 @@
+package com.xworkz.oops.methodoverloading;
+
+public class human {
+	String haircolour;
+	String eyecolour;
+	String location;
+	String lang;
+	 Heart Heart;
+	
+	void see() {
+		System.out.println("Human eye is better than any camera");
+	}
+	void dark() {
+		System.out.println("Human eye can't see in the dark...!");
+		
+		
+		
+			
+			
+		
+	}
+
+}
