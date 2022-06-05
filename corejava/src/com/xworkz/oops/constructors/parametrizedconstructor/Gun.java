523a7b02175b99f89443ae04ac5721aa1b69b266
User <raghavendra123xworkz@gmail.com>
2022-06-05 19:44:42 +0530

    example for paramaterized constructer

---
+++ corejava/src/com/xworkz/oops/constructors/parametrizedconstructor/Gun.java
@@ -0,0 +1,16 @@
+package com.xworkz.oops.constructors.parametrizedconstructor;
+
+public class Gun {
+	int noofbullet;
+	double weight;
+	String colour;
+	
+	Gun(int bullet,Double wt,String clr){
+	noofbullet=bullet;
+	weight=wt;
+	colour=clr;
+	
+	
+	}
+
+}
---
+++ corejava/src/com/xworkz/oops/constructors/parametrizedconstructor/paramaterizedconstructerdemo.java
@@ -0,0 +1,56 @@
+package com.xworkz.oops.constructors.parametrizedconstructor;
+
+public class paramaterizedconstructerdemo 
+{
+	
+public static void main(String[] args) 
+{
+
+Gun gun =new Gun(4,1.5,"Black");
+
+
+
+System.out.println(gun.colour);
+System.out.println("Gun Details:");
+System.out.println("gun.noofbullet:"+gun.noofbullet);
+System.out.println("weght:"+gun.weight);
+System.out.println("colour:"+gun.colour);
+
+
+
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
