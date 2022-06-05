cb7ad27c64fb0decc69f9d2b81f7ba7115aec72e
User <raghavendra123xworkz@gmail.com>
2022-06-05 19:46:03 +0530

    example for Default constructer

---
+++ corejava/src/com/xworkz/oops/constructors/gun.java
@@ -0,0 +1,19 @@
+package com.xworkz.oops.constructors;
+
+public class gun {
+	gun(){
+		System.out.println("Default constructer");
+	}	
+	//class Default constructer
+		public static void main(String[] args) {
+			gun gun =new gun();
+			
+			
+			
+		}
+	
+	
+	
+	}
+
+
