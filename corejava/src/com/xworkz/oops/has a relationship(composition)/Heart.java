29aa34d36f59be201d5d6326ffbdffd0eae7c63f
User <raghavendra123xworkz@gmail.com>
2022-06-06 14:40:13 +0530

    has a relationship example

---
+++ corejava/src/com/xworkz/oops/has a relationship(composition)/Heart.java
@@ -0,0 +1,17 @@
+package com.xworkz.oops.methodoverloading;
+
+public class Heart {
+	String colour;
+	String btimes;
+	
+	void pumpblood() {
+		System.out.println("Heart pumps blood to all organs");
+	}
+	
+	void receiveblood (){
+		System.out.println("Heart recives blood from blue veins..!");
+		
+		
+	}
+
+}
